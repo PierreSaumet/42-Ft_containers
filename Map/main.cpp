05a795d56f3fdb04d5ab717284d6171b6bc1b357
user42 <psaumet@student.42.fr>
2022-01-10 21:26:34 +0100

    delete useless line in the makefile and delete useless test in the main.cpp

--- Map/main.cpp
+++ Map/main.cpp
@@ -1558,184 +1558,60 @@ void	ft_display( void )
 	return ;
 }
 
-#include <list>
-#define T1 char
-#define T2 int
-
-
-
-template <typename T>
-class foo {
-	public:
-		typedef T	value_type;
-
-		foo( void ) : value(), _verbose(false) { };
-		foo(value_type src, const bool verbose = false) : value(src), _verbose(verbose) { };
-		foo(foo const &src, const bool verbose = false) : value(src.value), _verbose(verbose) { };
-		~foo( void ) { if (this->_verbose) std::cout << "~foo::foo()" << std::endl; };
-		void m( void ) { std::cout << "foo::m called [" << this->value << "]" << std::endl; };
-		void m( void ) const { std::cout << "foo::m const called [" << this->value << "]" << std::endl; };
-		foo &operator=(value_type src) { this->value = src; return *this; };
-		foo &operator=(foo const &src) {
-			if (this->_verbose || src._verbose)
-				std::cout << "foo::operator=(foo) CALLED" << std::endl;
-			this->value = src.value;
-			return *this;
-		};
-		value_type	getValue( void ) const { return this->value; };
-		void		switchVerbose( void ) { this->_verbose = !(this->_verbose); };
-
-		operator value_type( void ) const {
-			return value_type(this->value);
-		}
-	private:
-		value_type	value;
-		bool		_verbose;
-};
-typedef std::pair<const char, foo<int>> T3;
-
-template <typename T>
-std::string	printPair(const T &iterator, bool nl = true, std::ostream &o = std::cout)
-{
-	o << "key: " << iterator->first << " | value: " << iterator->second;
-	if (nl)
-		o << std::endl;
-	return ("");
-}
-
-template <typename T_MAP>
-void	printSize(T_MAP const &mp, bool print_content = 1)
-{
-	std::cout << "size: " << mp.size() << std::endl;
-	std::cout << "max_size: " << mp.max_size() << std::endl;
-	if (print_content)
-	{
-		typename T_MAP::const_iterator it = mp.begin(), ite = mp.end();
-		std::cout << std::endl << "Content is:" << std::endl;
-		for (; it != ite; ++it)
-			std::cout << "- " << printPair(it, false) << std::endl;
-	}
-	std::cout << "###############################################" << std::endl;
-}
-template <typename T>
-T	dec(T it, int n)
-{
-	while (n-- > 0)
-		--it;
-	return (it);
-}
-template <typename T>
-T	inc(T it, int n)
-{
-	while (n-- > 0)
-		++it;
-	return (it);
-}
-
-// template <typename char, typename int>
-void	printReverse(ft::map<char, int> &mp)
-{
-	//typename 
-	ft::map<char, int>::iterator it = mp.end();
-	ft::map<char, int>::iterator ite = mp.begin();
-
-	std::cout << "printReverse:" << std::endl;
-	while (it != ite) {
-		it--;
-		std::cout << "-> " << printPair(it, false) << std::endl;
-	}
-	std::cout << "_______________________________________________" << std::endl;
-}
 
 int     main( void )
 {
-	std::list<T3> lst;
-	unsigned int lst_size = 5;
-	for (unsigned int i = 0; i < lst_size; ++i)
-		lst.push_back(T3(2.5 + i, i + 1));
+	int		ret = 0;
+	while (ret != -1)
+	{
+		std::cout << std::endl;
+		ft_display();
+		std::cin >> ret;
+		if (ret == 1)
+			ft_all();
+		else if (ret == 2)
+		{
+			ft_begin_end();
+			ft_rbegin_rend();
+		}
+		else if (ret == 3)
+		{
+			ft_empty();
+			ft_size();
+			ft_max_size();
+		}
+		else if (ret == 4)
+			ft_operator();
+		else if (ret == 5)
+		{
+			ft_insert();
+			ft_erase();
+			ft_swap();
+			ft_clear();
+		}
+		if (ret == 6)
+		{
+			ft_key_comp();
+			ft_value_comp();
+		}
+		else if (ret == 7)
+		{
+			ft_find();
+			ft_count();
+			ft_lower_upper();
+			ft_equal_range();
+		}
+		else if (ret == 8)
+		{
+			ft_get_allocator();
+		}
+		else if (ret == 9)
+		{
+			ft_constructor();
+		}
+		else
+			ret = -1;
 
-	std::map<T1, T2> mp(lst.begin(), lst.end());
-	std::map<T1, T2>::iterator it(mp.begin());
-	std::map<T1, T2>::const_iterator ite(mp.begin());
-	printSize(mp);
-
-	printPair(++ite);
-	printPair(ite++);
-	printPair(ite++);
-	printPair(++ite);
-
-	it->second.m();
-	ite->second.m();
-
-	printPair(++it);
-	printPair(it++);
-	printPair(it++);
-	printPair(++it);
-
-	printPair(--ite);
-	printPair(ite--);
-	printPair(--ite);
-	printPair(ite--);
-
-	(*it).second.m();
-	(*ite).second.m();
-
-	printPair(--it);
-	printPair(it--);
-	printPair(it--);
-	printPair(--it);
-
-	// int		ret = 0;
-	// while (ret != -1)
-	// {
-	// 	std::cout << std::endl;
-	// 	ft_display();
-	// 	std::cin >> ret;
-	// 	if (ret == 1)
-	// 		ft_all();
-	// 	else if (ret == 2)
-	// 	{
-	// 		ft_begin_end();
-	// 		ft_rbegin_rend();
-	// 	}
-	// 	else if (ret == 3)
-	// 	{
-	// 		ft_empty();
-	// 		ft_size();
-	// 		ft_max_size();
-	// 	}
-	// 	else if (ret == 4)
-	// 		ft_operator();
-	// 	else if (ret == 5)
-	// 	{
-	// 		ft_insert();
-	// 		ft_erase();
-	// 		ft_swap();
-	// 		ft_clear();
-	// 	}
-	// 	if (ret == 6)
-	// 	{
-	// 		ft_key_comp();
-	// 		ft_value_comp();
-	// 	}
-	// 	else if (ret == 7)
-	// 	{
-	// 		ft_find();
-	// 		ft_count();
-	// 		ft_lower_upper();
-	// 		ft_equal_range();
-	// 	}
-	// 	else if (ret == 8)
-	// 	{
-	// 		ft_get_allocator();
-	// 	}
-	// 	else if (ret == 9)
-	// 	{
-	// 		ft_constructor();
-	// 	}
-	// 	else
-	// 		ret = -1;
-
-	// }
+	}
 	return (0);
 }
